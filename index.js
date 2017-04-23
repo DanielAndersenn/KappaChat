e397340d8383b7769ed1130e5548a811e4ad4487
DonHaugaard <s144686@student.dtu.dk>
2017-04-23 16:14:22 +0200

    Added scripts folder and moved emote logic to its own .js file

--- index.js
+++ index.js
@@ -17,38 +17,3 @@ io.on('connection', function(socket){
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
-
-// request is a node.js module that makes us capable of making http calls
-var request = require("request");
-
-// Defines the URL from which the emotes are fetdched
-var url = " https://twitchemotes.com/api_cache/v2/global.json";
-
-// Define request function
-request({url: url, json: true},
-   function (error, response, body) { // body is out .json object
-
-     // Javascript object
-     var obj = {
-       table: []
-     };
-
-     // Fetch url
-     if (!error && response.statusCode === 200) { // If no error occurs...
-
-       for (x in body) { // For each element in the .json (body)
-         //document.getElementById("demo").innerHTML += myObj[x];
-         console.log(body[x]);
-         // Write element x to obj
-         obj.table.push(body[x]);
-       }
-
-      // Convert object to string
-      var json = JSON.stringify(obj);
-
-      // Make file
-      fs.writeFile('myjsonfile.json', json, 'utf8');
-
-        // console.log(body) // Print the json response
-    }
-});
--- server.js
+++ server.js
@@ -3,16 +3,34 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var soap = require('soap');
 
-
-app.get('/', function(req, res){
+// '/ . . .' is the url name
+app.get('/login', function(req, res) {
    res.sendFile(__dirname + '/login.html');
- });
+});
 
-app.post('/', function(req, res) {
+app.get('/', function(req, res) {
+   res.redirect('/login');
+});
+
+// I guess it is not correct practice to make this get
+// since users then just can type localhost:3000/chat to enter chat..
+app.get('/chat', function(req, res) {
+   res.sendFile(__dirname + '/chat.html');
+
+   /*
+   If we had a user object we could have a boolean (isOnline)
+   that is set to 'true' if the user is online (i.e. after they have logged in)
+   and false othwerwise.
+   If the user then tries to type this direct URL in they will be redirected
+   to /login instead
+   */
+});
+
+app.post('/login', function(req, res) {
   //res.send();
 });
 
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
@@ -39,10 +57,23 @@ function authenticate(username, password) {
   var url = 'http://javabog.dk:9901/brugeradmin?wsdl';
   var args = {':arg0': username, ':arg1': password};
 
-  soap.createClient(url, function(err, client) {
+  soap.createClient(url, function(err, client, app) {
     client.BrugeradminImplService.BrugeradminImplPort.hentBruger(args, function(err, result) {
         console.log(result);
-    });
-  });
 
-}
+        // If no error redirect to chat
+        if (!err) {
+
+            Redirect();
+        }
+
+    }); // client end
+  }); // createClient end
+
+} // function end
+
+// TODO Should probably be kept in another file (?)
+// Function to redirect user on succesful login
+function Redirect(res) {
+  res.redirect('/chat');
+} // function end
