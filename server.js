82dfa8a53d84bed7a01412dd12e3baf09d1779df
DanielAndersenn <dsa-1992@hotmail.com>
2017-04-25 11:11:25 +0200

    Please no nodemodulens

--- server.js
+++ server.js
@@ -4,45 +4,82 @@ var io = require('socket.io')(http);
 var soap = require('soap');
 
 
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+
+//Entrypoint for klienter. Vi modtager et request (req) og tilbagesender et response (res)
 app.get('/', function(req, res){
    res.sendFile(__dirname + '/login.html');
  });
 
-app.post('/', function(req, res) {
-  //res.send();
-});
+ /*
+ app.post('/', function(req, res) {
+   res.sendFile(__dirname + '/chat.html');
+ })
+ */
 
+//Metoden bliver kaldt når en klient forbinder gennem websocket gennem javascript
 io.on('connection', function(socket){
+
+  //Metoden "aktiverer" når en klient sender en .emit med navnet 'chat message'
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
 
-socket.on('login', function(msg) {
+  //Metoden "aktiverer" når en klient sender en .emit med navnet 'login'
+  socket.on('login', function(msg) {
+  //Split stringen op på tegnet '|' for at separere user/pass.
   var attributes = msg.split('|');
 
   var username = attributes[0];
   var pass = attributes[1];
 
   console.log('Trying to log in user with Username: ' + username + ' Pass: ' + pass);
-  authenticate(username, pass);
+  //Kald metoden der authenticater mod javabog over SOAP
+  //var authenticated = authenticate(username, pass);
+
+  authenticate(username, pass, function(authenticated) {
+    console.log('Value of authenticated: ' + authenticated);
+    if(authenticated)
+    {
+    var destination = '/chat.html';
+    socket.emit('authenticated', destination)
+    }
+    else
+      {
+        //sendFile(__dirname + '/login.html');
+        console.log('Test?');
+      }
+  });
+
+
 
 });
 
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
 
-function authenticate(username, password) {
+function authenticate(username, password, callback) {
 
   var url = 'http://javabog.dk:9901/brugeradmin?wsdl';
   var args = {':arg0': username, ':arg1': password};
 
   soap.createClient(url, function(err, client) {
     client.BrugeradminImplService.BrugeradminImplPort.hentBruger(args, function(err, result) {
-        console.log(result);
+        if(err==null)
+        {
+          console.log(result);
+        callback(true);
+        }else
+        {
+        console.log(err);
+        callback(false);
+        }
+
     });
   });
+
 
 }
